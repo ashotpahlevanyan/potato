5168d5f63f8a42082788b515edaf88f8390fcba1
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2015-09-24 01:32:16 +0200

    list, and navigation to detail

--- app/app.js
+++ app/app.js
@@ -1,17 +1,29 @@
- app = angular.module('potatoApp', ['ngRoute']);
+app = angular.module('potatoApp', ['ngRoute', 'angular-momentjs']);
 
+app.config(function ($momentProvider){
+    $momentProvider
+      .asyncLoading(false)
+      .scriptUrl('//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min.js');
+  });
+
+app.config(function($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.withCredentials = true;
+    //$httpProvider.defaults.Access-Control-Allow-Credentials = true;
+});
 
 //This configures the routes and associates each route with a view and a controller
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/list',
             {
-                controller: 'ListController',
+                controller: 'ImageListController',
                 templateUrl: '/app/partials/list.html'
             })
-        .when('/detail',
+        .when('/detail/:itemID',
             {
-                controller: 'DetailController',
+                controller: 'ImageDetailController',
                 templateUrl: '/app/partials/detail.html'
             })
         .otherwise({ redirectTo: '/list' });
